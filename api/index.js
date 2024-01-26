80aa812632a931e6892265598c774834d66578b5
Aman Kumar <aman.kr.2129@gmail.com>
2024-01-26 22:41:37 +0530

    added route to fetch photo location

--- api/index.js
+++ api/index.js
@@ -17,7 +17,11 @@ app.get("/api", (req, res) => {
 
 app.get("/api/wallpaper", async (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.setHeader("Cache-Control", "s-max-age=3600, stale-while-revalidate");
+  res.setHeader(
+    "Cache-Control",
+    "public, s-max-age=86400, stale-while-revalidate"
+  );
+
   try {
     const collectionId = req.query.collection_id || "Ql7C2dPpjkw";
     const unsplashApi = `https://api.unsplash.com/collections/${collectionId}/photos?client_id=${UNSPLASHAPIKEY}`;
@@ -28,6 +32,29 @@ app.get("/api/wallpaper", async (req, res) => {
 
     const wallpapers = await wallpaperResponse.json();
     res.json(wallpapers);
+  } catch (err) {
+    console.error("Error fetching location data:", err);
+  }
+});
+
+app.get("/api/location", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader(
+    "Cache-Control",
+    "public, s-max-age=86400, stale-while-revalidate"
+  );
+
+  const id = req.query.id;
+
+  try {
+    const unsplashApi = `https://api.unsplash.com/photos/${id}?client_id=${UNSPLASHAPIKEY}`;
+    const wallpaperResponse = await fetch(unsplashApi);
+
+    if (!wallpaperResponse.ok)
+      throw new Error(`HTTP error! Status: ${wallpaperResponse.status}`);
+
+    const wallpaper = await wallpaperResponse.json();
+    res.json({ id: wallpaper.id, location: wallpaper.location });
   } catch (err) {
     console.error("Error fetching location data:", err);
   }
